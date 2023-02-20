d3e11c1898680178aa467707c856b3a47d53cd4a
Shubham Goyal <shubhamgoyal7125@gmail.com>
2023-02-21 01:36:05 +0530

    Navbar updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,19 +7,48 @@ import '../styles/navbar.css';
 const Navbar = () => {
     return (
 
+        // <>
+        //     <div classNameName='navbar'>
+        //         <img src={logo} alt="img" classNameName='logo_img'/>
+
+        //         <div classNameName='navbar_links'>
+        //             <NavLink classNameName="navbar-link" to="/">Home</NavLink>
+        //             <NavLink classNameName="navbar-link" to="/">Events</NavLink>
+        //             <NavLink classNameName="navbar-link" to="/">Sponsors</NavLink>
+        //             <NavLink classNameName="navbar-link" to="/">Legacy</NavLink>
+        //             <NavLink classNameName="navbar-link" to="/">Contact Us</NavLink>
+        //         </div>
+
+        //     </div>
+        // </>
         <>
-            <div className='navbar'>
-                <img src={logo} alt="img" className='logo_img'/>
-
-                <div className='navbar_links'>
-                    <NavLink className="navbar-link" to="/">Home</NavLink>
-                    <NavLink className="navbar-link" to="/">Events</NavLink>
-                    <NavLink className="navbar-link" to="/">Sponsors</NavLink>
-                    <NavLink className="navbar-link" to="/">Legacy</NavLink>
-                    <NavLink className="navbar-link" to="/">Contact Us</NavLink>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    <img src={logo} alt="img" classNameName='navbar-brand' width="120" height="130"/>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+                        <ul className="navbar-nav pe-5">
+                            <li className="nav-item ps-4 text-bold">
+                                <NavLink className="nav-link active" aria-current="page" to="#">Home</NavLink>
+                            </li>
+                            <li className="nav-item ps-4 text-bold">
+                                <NavLink className="nav-link" to="#">Events</NavLink>
+                            </li>
+                            <li className="nav-item ps-4 text-bold">
+                                <NavLink className="nav-link" to="#">Sponsors</NavLink>
+                            </li>
+                            <li className="nav-item ps-4 text-bold">
+                                <NavLink className="nav-link" to="#">Team</NavLink>
+                            </li>
+                            <li className="nav-item ps-4 text-bold">
+                                <NavLink className="nav-link" to="#">Legacy</NavLink>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-
-            </div>
+            </nav>
         </>
     );
 }
