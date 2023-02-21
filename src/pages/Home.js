ed2c9cf0a07a1849e51077e3dc45330a6ac43d1e
Shubham Goyal <shubhamgoyal7125@gmail.com>
2023-02-21 15:59:52 +0530

    minor updates

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "../styles/home.css";
 import arman_malik from "../images/am3.png";
-import arrow from '../images/arrow.gif';
 
 
 const Home = () => {
@@ -13,10 +12,6 @@ const Home = () => {
                     <h1 className='home_page_label_anugoonj'>ANUGOONJ</h1>
                 </div>
                 <img src={arman_malik} alt="img" className='home_arman_malik_image'/>
-
-                <div className='home_arrow'>
-                    <img src={arrow} alt="arrow" className='home_down_arrow' />
-                </div>
             </div>
         </div>
     );
