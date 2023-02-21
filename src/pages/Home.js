33c8f8a7f72313cf76dde19a48fc0a28882cd066
singlasonu2020 <ssgroup.net2022@gmail.com>
2023-02-21 13:59:18 +0530

    navbar and home 2

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "../styles/home.css";
+import arman_malik from "../images/arman_malik.png";
 
 
 const Home = () => {
     return(
         <div className='home'>
             <div className='home_inside'>
+                <img src={arman_malik} alt="img" className='home_arman_malik_image'/>
             </div>
         </div>
     );
