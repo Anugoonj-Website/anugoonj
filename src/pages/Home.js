6b207c9b046abf0d240cf7cc96877726c3db8e21
Shubham Goyal <shubhamgoyal7125@gmail.com>
2023-02-21 22:11:25 +0530

    Added about us background

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,15 +4,28 @@ import arman_malik from "../images/am3.png";
 
 
 const Home = () => {
-    return(
+    return (
         <div className='home'>
             <div className='home_inside'>
                 <spam className='home_page_label'>
                     <h1 className='home_page_label_anugoonj'>ANUGOONJ</h1>
                 </spam>
-                <img src={arman_malik} alt="img" className='home_arman_malik_image'/>
+                <img src={arman_malik} alt="img" className='home_arman_malik_image' />
             </div>
             <div className='home_about_us'>
+                
+
+                <div class="animation-area">
+                <h1 className='home_page_label_anugoonj'>About Us</h1>
+                    <ul class="box-area">
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </div>
 
             </div>
         </div>
