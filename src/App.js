081ab243291750e564baccdaaaaf8fd421f16633
RishabhXD <rishabhpant14@gmail.com>
2023-02-21 15:28:42 +0530

    routes fix

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import './App.css';
+import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from './components/Navbar'
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <>
-        <Router>
-          <Navbar/>
-          <Routes>
-            <Route path="/home" element={<Home/>} />
-          </Routes>
-        </Router>
-
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </>
   );
 }
