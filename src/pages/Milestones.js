bf3fb1e4c583be3df96b0ef5acb085cb1d746745
Toyesh Sinha <sinha-toyeesh>
2023-02-22 04:38:15 +0530

    Update Milestones

--- src/pages/Milestones.js
+++ src/pages/Milestones.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
 import "../styles/milestones.css";
 
 const Milestones = () => {
@@ -21,26 +22,74 @@ const Milestones = () => {
 
         <div class="row">
           <div class="column">
-            <div class="text-year">
-              <div class="button">
-                <button type="submit">2020</button>
-              </div>
+            <div class="button">
+              <button type="submit" class="2020-btn">
+                2020
+              </button>
             </div>
           </div>
           <div class="column">
-          <div class="text-year">
-              <div class="button">
-                <button type="submit">2019</button>
-              </div>
+            <div class="button">
+              <button type="submit" class="2019-btn">
+                2019
+              </button>
             </div>
           </div>
           <div class="column">
-          <div class="text-year">
-              <div class="button">
-                <button type="submit">2018</button>
-              </div>
+            <div class="button">
+              <button type="submit" class="2018-btn">
+                2018
+              </button>
             </div>
           </div>
+        </div>
+
+        <div
+          id="carouselExampleControls"
+          class="carousel slide"
+          data-ride="carousel"
+        >
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img
+                class="d-block w-100"
+                src="/src/images/_MG_1409.JPG"
+                alt="First slide"
+              ></img>
+            </div>
+            <div class="carousel-item">
+              <img
+                class="d-block w-100"
+                src="/src/images/_MG_1409.JPG"
+                alt="Second slide"
+              ></img>
+            </div>
+            <div class="carousel-item">
+              <img
+                class="d-block w-100"
+                src="/src/images/_MG_1409.JPG"
+                alt="Third slide"
+              ></img>
+            </div>
+          </div>
+          <a
+            class="carousel-control-prev"
+            href="#carouselExampleControls"
+            role="button"
+            data-slide="prev"
+          >
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a
+            class="carousel-control-next"
+            href="#carouselExampleControls"
+            role="button"
+            data-slide="next"
+          >
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
         </div>
       </div>
     </div>
