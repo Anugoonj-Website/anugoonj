d12e845c449c574f0b163f8ea0929c1dcdac953e
singlasonu2020 <ssgroup.net2022@gmail.com>
2023-02-22 14:28:55 +0530

    event page updates

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -40,27 +40,4 @@ const Home = () => {
   );
 };
 
-<<<<<<< HEAD
-                <div class="animation-area">
-                {/* <h1 className='home_page_label_anugoonj'>About Us</h1>
-                    <ul class="box-area">
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                    </ul> */}
-                    <img src="https://tenor.com/view/dance-chicago-dance-crash-hip-hop-dance-tron-gif-14021039"/>
-                    {/* <div class="tenor-gif-embed" data-postid="14021039" data-share-method="host" data-aspect-ratio="1.77778" data-width="100%"><a href="https://tenor.com/view/dance-chicago-dance-crash-hip-hop-dance-tron-gif-14021039">Dance Chicago Dance Crash GIF</a>from <a href="https://tenor.com/search/dance-gifs">Dance GIFs</a></div> <script type="text/javascript" async src="https://tenor.com/embed.js"></script> */}
-                </div>
-
-            </div>
-        </div>
-    );
-}
-
-export default Home
-=======
 export default Home;
->>>>>>> 4a6ea035a52b8404d0d435fff6acfc665754cdcc
