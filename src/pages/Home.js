4139aa22b1d6eb6a91aabf0868f76924443b0c88
singlasonu2020 <ssgroup.net2022@gmail.com>
2023-02-22 11:20:16 +0530

    add event page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,7 +16,7 @@ const Home = () => {
                 
 
                 <div class="animation-area">
-                <h1 className='home_page_label_anugoonj'>About Us</h1>
+                {/* <h1 className='home_page_label_anugoonj'>About Us</h1>
                     <ul class="box-area">
                         <li></li>
                         <li></li>
@@ -24,7 +24,9 @@ const Home = () => {
                         <li></li>
                         <li></li>
                         <li></li>
-                    </ul>
+                    </ul> */}
+                    <img src="https://tenor.com/view/dance-chicago-dance-crash-hip-hop-dance-tron-gif-14021039"/>
+                    {/* <div class="tenor-gif-embed" data-postid="14021039" data-share-method="host" data-aspect-ratio="1.77778" data-width="100%"><a href="https://tenor.com/view/dance-chicago-dance-crash-hip-hop-dance-tron-gif-14021039">Dance Chicago Dance Crash GIF</a>from <a href="https://tenor.com/search/dance-gifs">Dance GIFs</a></div> <script type="text/javascript" async src="https://tenor.com/embed.js"></script> */}
                 </div>
 
             </div>
