acee7ab4cb124c8ee5f1f5413616ba77fe3afac9
AryavirMalik07 <aryavirmalik20@gmail.com>
2023-02-22 22:02:18 +0530

    color change

--- src/pages/Milestones.js
+++ src/pages/Milestones.js
@@ -56,25 +56,25 @@ const Milestones = () => {
             </div>
           </div>
         </div>
-        <Carousel
-          autoPlay
-          infiniteLoop
-          showStatus={false}
-          showIndicators={false}
-          showThumbs={false}
-          interval={5000}
-        >
-          <div>
-            <img loading="lazy" src="https://links.papareact.com/gi1" alt="" />
-          </div>
-          <div>
-            <img loading="lazy" src="https://links.papareact.com/6ff" alt="" />
-          </div>
-          <div>
-            <img loading="lazy" src="https://links.papareact.com/7ma" alt="" />
-          </div>
-        </Carousel>
       </div>
+      <Carousel
+        autoPlay
+        infiniteLoop
+        showStatus={false}
+        showIndicators={false}
+        showThumbs={false}
+        interval={5000}
+      >
+        <div>
+          <img loading="lazy" src="https://links.papareact.com/gi1" alt="" />
+        </div>
+        <div>
+          <img loading="lazy" src="https://links.papareact.com/6ff" alt="" />
+        </div>
+        <div>
+          <img loading="lazy" src="https://links.papareact.com/7ma" alt="" />
+        </div>
+      </Carousel>
     </div>
   );
 };
