b62e27f209d768051632f6b492f6600daf195462
Shubham Goyal <shubhamgoyal7125@gmail.com>
2023-02-22 21:44:26 +0530

    Updated event page

--- src/components/EventLeft.js
+++ src/components/EventLeft.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import '../styles/event.css'
+import '../styles/eventLeft.css'
 import '../styles/circle.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLocationDot ,faClock} from '@fortawesome/free-solid-svg-icons'
@@ -13,10 +13,11 @@ const Event = (props) => {
        {/* <div class="box">
         <img src={temp}/>
        </div> */}
+
        <img src={temp} className="image"/>
           <div className='event_content event_conent_bondary'>
-          <h1 className='event_heading'>event1</h1>
-          <p className='event_paragraph'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate dolores epturi.Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate dolores epturi</p>
+          <h1 className='event_heading'>Event1</h1>
+          <p className='event_paragraph'>Lorem ipsum dolor sit amet Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque, expedita! consectetur adipisicing elit. Voluptate dolores epturi.Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate dolores epturi</p>
           <div className='location_time'>
             <div><FontAwesomeIcon icon={faLocationDot}/> Location : D-Block</div>
             <div className='event_time'><FontAwesomeIcon icon={faClock}/> Time : 2:30 Pm</div>
---
+++ src/components/EventRight.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import '../styles/eventLeft.css'
+import '../styles/circle.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLocationDot ,faClock} from '@fortawesome/free-solid-svg-icons'
+import temp from "../images/temp2.png";
+
+
+const Event = (props) => {
+  return (
+    <div className='left_event_body'>
+       
+          <div className='event_content event_conent_bondary' style={{marginLeft:"0", marginRight: "2vw"}}>
+          <h1 className='event_heading'>Event1</h1>
+          <p className='event_paragraph'>Lorem ipsum dolor sit amet Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque, expedita! consectetur adipisicing elit. Voluptate dolores epturi.Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate dolores epturi</p>
+          <div className='location_time'>
+            <div><FontAwesomeIcon icon={faLocationDot}/> Location : D-Block</div>
+            <div className='event_time'><FontAwesomeIcon icon={faClock}/> Time : 2:30 Pm</div>
+          </div>
+        </div>
+        <img src={temp} className="image"/>
+       
+    </div>
+  )
+}
+
+export default Event   
