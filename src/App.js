e8719bdd7bd15964280afb476cbe4e716f2065eb
AryavirMalik07 <aryavirmalik20@gmail.com>
2023-02-23 18:28:39 +0530

    footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import "./App.css";
-import 'bootstrap/dist/css/bootstrap.css';
-import { BrowserRouter as Router, Routes, Route,Navigate } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Events from "./pages/Events";
@@ -9,6 +14,7 @@ import Sponsor from "./pages/Sponsor";
 import Team from "./pages/Team";
 import Contact from "./pages/Contact";
 import Milestones from "./pages/Milestones";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -17,16 +23,26 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/events" element={<Events />} >
+          <Route path="/events" element={<Events />}>
             <Route index element={<Navigate to="day_1" />} />
-            <Route path="day_1" element={<Events_Component day="1" date="2nd"/>}/>
-            <Route path="day_2" element={<Events_Component day="2" date="3rd"/>}/>
-            <Route path="day_3" element={<Events_Component day="3" date="4th"/>}/>
+            <Route
+              path="day_1"
+              element={<Events_Component day="1" date="2nd" />}
+            />
+            <Route
+              path="day_2"
+              element={<Events_Component day="2" date="3rd" />}
+            />
+            <Route
+              path="day_3"
+              element={<Events_Component day="3" date="4th" />}
+            />
           </Route>
           <Route path="/sponsors" element={<Sponsor />} />
-          <Route path="/team" element={<Team/>} />
+          <Route path="/team" element={<Team />} />
           <Route path="/milestones" element={<Milestones />} />
         </Routes>
+        <Footer />
       </Router>
     </>
   );
--- src/components/EachSponsor.js
+++ src/components/EachSponsor.js
@@ -1,17 +1,16 @@
-import React from 'react'
-import neon_frame from '../images/neon,frame,border,neon frame,neon border,neon shape,neon frame png,neon line,neon lights,light,pink,blue,neon pink,neon png frame,rectangle,neon gradient,neon frame free png,neon border free png,free neon frame,neo.png'
-import coca_cola from '../images/sponsors_image/coca_cola.png'
-import '../styles/eachSponsor.css'
+import React from "react";
+// import neon_frame from "../images/neon,frame,border,neon frame,neon border,neon shape,neon frame png,neon line,neon lights,light,pink,blue,neon pink,neon png frame,rectangle,neon gradient,neon frame free png,neon border free png,free neon frame,neo.png";
+import coca_cola from "../images/sponsors_image/coca_cola.png";
+import "../styles/eachSponsor.css";
 
 const EachSponsor = () => {
   return (
     <div>
-        <div className="eachSponsorComponent">
-            <img src={coca_cola} alt="img" className='spon_image'/>
-        </div>
-
+      <div className="eachSponsorComponent">
+        <img src={coca_cola} alt="img" className="spon_image" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default EachSponsor
+export default EachSponsor;
