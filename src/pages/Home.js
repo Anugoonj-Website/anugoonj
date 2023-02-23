3840db0cf1f2a996d57148d64a11febfcb1aafa4
Ayush Baliyan <ayush.3316403221@ipu.ac.in>
2023-02-23 23:07:09 +0530

    Added the stars section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,6 +8,39 @@ import Fullpage, {
   FullpageSection,
 } from "@ap.cx/react-fullpage";
 
+const starsArray = [
+  {
+  Date: "03-03-2023",
+  Name: "Guest Star Present",
+  Desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit natus, eveniet veniam nemo cumque esse dignissimos aliquam assumenda inventore dicta. Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, dolores. Optio, ipsam? Est, optio quis magnam autem voluptates fugiat! At.",
+  image:"artist_image1.png",
+  SubStars: ["Mohit Chauhan", "Mohit Chauhan", "Mohit Chauhan", "Mohit Chauhan"]
+  },
+  {
+  Date: "03-03-2023",
+  Name: "Guest Star Present",
+  Desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit natus, eveniet veniam nemo cumque esse dignissimos aliquam assumenda inventore dicta. Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, dolores. Optio, ipsam? Est, optio quis magnam autem voluptates fugiat! At.",
+  image:"artist_image2.png",
+  SubStars: ["Mohit Chauhan", "Mohit Chauhan", "Mohit Chauhan", "Mohit Chauhan"]
+  },
+  {
+  Date: "03-03-2023",
+  Name: "Guest Star Present",
+  Desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit natus, eveniet veniam nemo cumque esse dignissimos aliquam assumenda inventore dicta. Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, dolores. Optio, ipsam? Est, optio quis magnam autem voluptates fugiat! At.",
+  image:"artist_image1.png",
+  SubStars: ["Mohit Chauhan", "Mohit Chauhan", "Mohit Chauhan", "Mohit Chauhan"]
+  },
+  {
+    Date: "03-03-2023",
+    Name: "Guest Star Present",
+    Desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit natus, eveniet veniam nemo cumque esse dignissimos aliquam assumenda inventore dicta. Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, dolores. Optio, ipsam? Est, optio quis magnam autem voluptates fugiat! At.",
+    image:"artist_image2.png",
+    SubStars: ["Mohit Chauhan", "Mohit Chauhan", "Mohit Chauhan", "Mohit Chauhan"]
+  }
+]
+
+
+
 const Home = () => {
   return (
     <Fullpage>
@@ -41,11 +74,32 @@ const Home = () => {
           <h1>Guest Performance</h1>
         </FullpageSection>
         <FullpageSection style={{ height: "100vh " }}>
-          <h1>Page2</h1>
+          <div className="stars h-[100vh] pt-[20vh] flex flex-row-reverse">
+            <div className="music-box h-[90vh] relative right-[-7vw]">
+              <img src={require("../images/music-speaker.png")} className="h-[100%]" alt="" />
+            </div>
+            <div className="h-[100vh] mt-[-20vh] overflow-auto">
+              { starsArray.map((item) => (
+                <div className="flex flex-row translate-y-[20vh] items-start justify-center" key={item.image}>
+                <div className="left mr-16">
+                  {/* <img src={require("../images/neon-ring.png")} className="neon-ring" alt="" /> */}
+                  <img src={require(`../images/${item.image}`)} className="artist_image" alt="" />
+                </div>
+                <div className="right w-[30vw]">
+                  <div className="date"><h1 className=" font-bold">{item.Date}</h1></div>
+                  <div className="name"><h1 className=" font-bold">{item.Name}</h1></div>
+                  <div className="Desc"><p className="text-white">{item.Desc}</p></div>
+                </div>
+              </div>
+              ))}
+            </div>
+          </div>
         </FullpageSection>
       </FullPageSections>
     </Fullpage>
   );
 };
+
+
 
 export default Home;
