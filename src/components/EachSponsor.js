4b8e908ad74a673082d307b0bc20aecc45720b81
Ayush Baliyan <ayush.3316403221@ipu.ac.in>
2023-02-23 23:09:26 +0530

    Added the stars section

--- src/components/EachSponsor.js
+++ src/components/EachSponsor.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-// import neon_frame from "../images/neon,frame,border,neon frame,neon border,neon shape,neon frame png,neon line,neon lights,light,pink,blue,neon pink,neon png frame,rectangle,neon gradient,neon frame free png,neon border free png,free neon frame,neo.png";
-import coca_cola from "../images/sponsors_image/coca_cola.png";
-import "../styles/eachSponsor.css";
-
-const EachSponsor = () => {
-  return (
-    <div>
-      <div className="eachSponsorComponent">
-        <img src={coca_cola} alt="img" className="spon_image" />
-      </div>
-    </div>
-  );
-};
-=======
 import React from 'react'
 import coca_cola from '../images/sponsors_image/coca_cola.png'
 import '../styles/eachSponsor.css'
@@ -30,6 +14,5 @@ const EachSponsor = () => {
 
   )
 }
->>>>>>> cc8e22daa6135f3ff4af832f1ff5ec8f211b9e0a
 
 export default EachSponsor;
