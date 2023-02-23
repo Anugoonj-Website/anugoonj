8ea39a88d07cdbd48a65e972e643290735672f77
Shubham Goyal <shubhamgoyal7125@gmail.com>
2023-02-23 17:27:06 +0530

    Updated sponsor page

--- src/pages/Sponsor.js
+++ src/pages/Sponsor.js
@@ -27,6 +27,22 @@ const Sponsor = () => {
         <EachSponsor />
         <EachSponsor />
         </div>
+
+        <div class="animation-area">
+        <ul class="box-area">
+          <li ></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          
+        </ul>
+      </div>
         
     </div>
   );
