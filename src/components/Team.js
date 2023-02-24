61c1844bf7a90c18a5315b48c89ca8c2cfa013e6
singlasonu2020 <ssgroup.net2022@gmail.com>
2023-02-25 01:13:31 +0530

    update teams page

--- src/components/Team.js
+++ src/components/Team.js
@@ -2,14 +2,41 @@ import React from 'react'
 import "../styles/team.css";
 import Profile from '../components/Profile';
 
-function team() {
+function team(props) {
+    const core_memeber_data = props.data.core;
+    const memebers_data = props.data.memebers;
+
+    
     return (
-        <div className='team'>
+        <>
+
+        {
+            // ()=>{
+            //     const details =[];
+            //     // while(i<core_memeber_data.length)
+            //     // {
+            //     //     details.add()
+                    
+            //     // }
+            // }
+        }
+         <div className='team'>
             <Profile />
-            {/* <Profile />
-            <Profile /> */}
+            <Profile />
+            <Profile />
+            <Profile />
         </div>
+        </>
+       
     )
 }
+
+
+function createGroups(arr, numGroups) {
+    const perGroup = Math.ceil(arr.length / numGroups);
+    return new Array(numGroups)
+      .fill('')
+      .map((_, i) => arr.slice(i * perGroup, (i + 1) * perGroup));
+  }
 
 export default team;
