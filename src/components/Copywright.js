a7a94eb3672d187bb41116fd68f5ebf04674e4c9
Shubham Goyal <shubhamgoyal7125@gmail.com>
2023-02-24 13:58:37 +0530

    Made events page responsive

---
+++ src/components/Copywright.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Copywright = () => {
+  return (
+    <div>
+        <div className="footer1">
+            <h1>All Rights Reserved</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Copywright
