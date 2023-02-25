4eb4ad738dd38a6306ab2fd81dfc13c1ba50584d
singlasonu2020 <ssgroup.net2022@gmail.com>
2023-02-25 15:01:09 +0530

    event sponser team page complete

--- src/pages/Teams.js
+++ src/pages/Teams.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import "../styles/teams.css";
 import { NavLink, Outlet } from "react-router-dom";
-import profile from '../components/Profile';
+import { useMediaQuery } from '@react-hook/media-query';
+
 
 
 const Team = () => {
+
+  const isTablet = useMediaQuery('only screen and (max-width: 1050px)');
+  const isPhone = useMediaQuery('only screen and (max-width: 450px)');
+
+
   return (
     <div className='teams'>
       <h2 className='heading_teams'>The Coordination Collective</h2>
 
-      <div className='team_page_options'>
+      {!isTablet && <div className='team_page_options'>
         <NavLink className="navbar_link nav_link_teams" to="/team/organising">Organising Team</NavLink>
         <NavLink className="navbar_link nav_link_teams" to="/team/web">Web Team</NavLink>
         <NavLink className="navbar_link nav_link_teams" to="/team/disipline">Discipline Team</NavLink>
@@ -18,9 +24,44 @@ const Team = () => {
         <NavLink className="navbar_link nav_link_teams" to="/team/mr_and_ms_anugoonj">Mr and Ms Anugoonj Team</NavLink>
         <NavLink className="navbar_link nav_link_teams" to="/team/ground">Ground Team</NavLink>
         <NavLink className="navbar_link nav_link_teams" to="/team/media">Media Team</NavLink>
-      </div>
+      </div>}
+      {isTablet && !isPhone && <>
+        <div className='team_page_options'>
+          <NavLink className="navbar_link nav_link_teams" to="/team/organising">Organising Team</NavLink>
+          <NavLink className="navbar_link nav_link_teams" to="/team/web">Web Team</NavLink>
+          <NavLink className="navbar_link nav_link_teams" to="/team/disipline">Discipline Team</NavLink>
+          <NavLink className="navbar_link nav_link_teams" to="/team/hospitality">Hospitality Team</NavLink>
+          <NavLink className="navbar_link nav_link_teams" to="/team/office">Office Team</NavLink>
 
-      <Outlet/>
+        </div>
+        <div className='team_page_options'>
+          <NavLink className="navbar_link nav_link_teams" to="/team/mr_and_ms_anugoonj">Mr and Ms Anugoonj Team</NavLink>
+          <NavLink className="navbar_link nav_link_teams" to="/team/ground">Ground Team</NavLink>
+          <NavLink className="navbar_link nav_link_teams" to="/team/media">Media Team</NavLink>
+        </div>
+      </>}
+
+
+      {isPhone && <>
+        <div className='team_page_options'>
+          <NavLink className="navbar_link nav_link_teams" to="/team/organising">Organising Team</NavLink>
+          <NavLink className="navbar_link nav_link_teams" to="/team/web">Web Team</NavLink>
+          <NavLink className="navbar_link nav_link_teams" to="/team/disipline">Discipline Team</NavLink>
+
+
+        </div>
+        <div className='team_page_options'>
+          <NavLink className="navbar_link nav_link_teams" to="/team/office">Office Team</NavLink>
+          <NavLink className="navbar_link nav_link_teams" to="/team/mr_and_ms_anugoonj">Mr and Ms Anugoonj Team</NavLink>
+          <NavLink className="navbar_link nav_link_teams" to="/team/ground">Ground Team</NavLink>
+
+        </div>
+        <div className='team_page_options'>
+          <NavLink className="navbar_link nav_link_teams" to="/team/hospitality">Hospitality Team</NavLink>
+          <NavLink className="navbar_link nav_link_teams" to="/team/media">Media Team</NavLink>
+        </div>
+      </>}
+      <Outlet />
 
       <div class="animation-area">
         <ul class="box-area">
@@ -30,7 +71,7 @@ const Team = () => {
           <li></li>
           <li></li>
           <li></li>
-          <li></li> 
+          <li></li>
           <li></li>
           <li></li>
           <li></li>
@@ -39,9 +80,9 @@ const Team = () => {
       </div>
 
       <div className="event_footer">
-      <p class="sponsor_rights_text">
-        © 2023 Created By <b>Anugoonj</b> All Rights Reserved.
-      </p>
+        <p class="sponsor_rights_text">
+          © 2023 Created By <b>Anugoonj</b> All Rights Reserved.
+        </p>
       </div>
 
     </div>
