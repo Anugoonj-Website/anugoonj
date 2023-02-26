4e42a02e5798dbcf59d6d988e81647d236aaa3f3
singlasonu2020 <ssgroup.net2022@gmail.com>
2023-02-26 17:22:19 +0530

    v1.0.0 complete

--- src/App.js
+++ src/App.js
@@ -66,6 +66,7 @@ function App() {
             <Route path="disipline" element={<Team data={teams_data.disipline}/>} />
             <Route path="hospitality" element={<Team data={teams_data.hospitality}/>} />
             <Route path="office" element={<Team data={teams_data.office}/>} />
+            <Route path="mr_and_ms_anugoonj" element={<Team data={teams_data.office}/>} />
             <Route path="ground" element={<Team data={teams_data.ground}/>} />
             <Route path="media" element={<Team data={teams_data.media}/>} />
           </Route>
